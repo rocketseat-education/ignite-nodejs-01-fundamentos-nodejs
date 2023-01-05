2685d0c0b62c9c3c900d0b479165b8b5a918d7d6
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-05 17:12:11 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -20,14 +20,28 @@ import http from 'node:http'
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
     return res.end('Criação de usuário')
   }
 
