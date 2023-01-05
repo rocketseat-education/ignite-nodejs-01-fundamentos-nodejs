cdcdd10c3903b5428d54960e43b3d60bf821201b
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-05 19:08:55 -0300

    Streams no Node.js - Entendendo Streams no Node.js

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & Spotify
+
+// Importação de clientes via CSV (Excel)
+// 1gb - 1.000.000
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Inserção no banco de dados
+
+// 10mb/s -> 10.000
+
+// Readable Streams / Writable Streams
