8c813f2df8b3e9cfb0e0610fd36007dce8feb81e
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-05 19:20:54 -0300

    Streams no Node.js - Criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,13 +1,27 @@
-// Netflix & Spotify
+// Streams ->
 
-// Importação de clientes via CSV (Excel)
-// 1gb - 1.000.000
-// POST /upload import.csv
+// process.stdin
+//   .pipe(process.stdout)
 
-// 10mb/s - 100s
+import { Readable } from 'node:stream'
 
-// 100s -> Inserção no banco de dados
+class OneToHundredStream extends Readable {
+  index = 1
 
-// 10mb/s -> 10.000
+  _read() {
+    const i = this.index++
 
-// Readable Streams / Writable Streams
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+
+        this.push(buf)
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
