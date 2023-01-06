0799e1a7d47a3d0f5f7b8be014d8157be4a0dbdf
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 13:50:34 -0300

    Streams no Node.js - Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -28,8 +28,20 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -38,10 +50,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
