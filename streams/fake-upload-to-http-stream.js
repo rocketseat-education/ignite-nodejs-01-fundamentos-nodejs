0a11b6a87d11d08d27d5339930996f9c51bbf33b
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 13:40:50 -0300

    Streams no Node.js - Consumindo uma stream completa

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null)
       } else {
         const buf = Buffer.from(String(i))
@@ -21,4 +21,8 @@ class OneToHundredStream extends Readable {
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
+}).then(res => {
+  return res.text()
+}).then(data => {
+  console.log(data)
 })
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -14,10 +14,18 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-  return req
-    .pipe(new InverseNumberStream())
-    .pipe(res)
+const server = http.createServer(async (req, res) => {
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return res.end(fullStreamContent)
 })
 
 server.listen(3334)
