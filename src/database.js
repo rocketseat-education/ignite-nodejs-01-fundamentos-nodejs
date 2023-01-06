b42e603d3ac755b54d119b6e3b6a96d8b957b490
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 14:07:58 -0300

    Banco de dados - Criando banco de dados JSON

---
+++ src/database.js
@@ -0,0 +1,19 @@
+export class Database {
+  #database = {}
+
+  select(table) {
+    const data = this.#database[table] ?? []
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    return data
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -9,18 +10,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
