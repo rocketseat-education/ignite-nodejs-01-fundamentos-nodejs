6cbc72db4a14ad352e221f5e6444cb5767c8a37f
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 14:14:18 -0300

    Banco de dados - Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,6 +31,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
