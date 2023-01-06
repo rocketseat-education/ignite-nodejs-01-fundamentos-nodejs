78841f65c081ea13a23cba2f69ecd491680e48e4
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 17:07:59 -0300

    Rotas da aplicação - Atualização de registros

--- src/database.js
+++ src/database.js
@@ -37,6 +37,15 @@ export class Database {
     return data
   }
 
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
--- src/routes.js
+++ src/routes.js
@@ -32,6 +32,21 @@ export const routes = [
     }
   },
   {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email,
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
     method: 'DELETE',
     path: buildRoutePath('/users/:id'),
     handler: (req, res) => {
