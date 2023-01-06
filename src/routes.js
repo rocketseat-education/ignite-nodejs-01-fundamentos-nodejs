fda2da03161155840586b0166775ed35c875c9e3
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-06 14:40:11 -0300

    Rotas da aplicação - Separando rotas da aplicação

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: '/users',
+    handler: (req, res) => {
+      const users = database.select('users')
+
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    path: '/users',
+    handler: (req, res) => {
+      const { name, email } = req.body
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      }
+
+      database.insert('users', user)
+
+      return res.writeHead(201).end()
+    }
+  },
+]
--- src/server.js
+++ src/server.js
@@ -1,33 +1,19 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
-import { json } from './middlewares/json.js'
 
-const database = new Database()
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    return res.end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
